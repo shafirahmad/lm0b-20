c89bb190e90fd17f7ec14d9bda202b2a0bf2d5b3
Shafir Ahmad <shafir_ahmad@aiap.sg>
2021-03-27 22:25:12 +0800

    correct spelling

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ cx_Freeze.setup(
     name = "Mizatorian LM0B 20",
     version = "1.0",
     options = {"build_exe": {"packages": ["pygame", "random", "math", "sys"],  
-        "include_files": ["music.mid","bounce.wav","crash.wav","wall.wac","at01.ttf"]}},
+        "include_files": ["music.mid","bounce.wav","crash.wav","wall.wav","at01.ttf"]}},
     executables = exe
 ) 
